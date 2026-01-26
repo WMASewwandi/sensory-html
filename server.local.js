994a30ba4e334e79550097363f617bf0a6cad547
Avishka Sewwandi <wmavishkasewwandi@gmail.com>
2026-01-26 18:38:13 +0530

    .

--- server.local.js
+++ server.local.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Route for the home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/'));
 });
 
 // Start the server
